6f07cbbed7a0a57103d6d19b9188b3f2e80944cb
Katrina Robles <katrinarobles@live.com>
2020-10-27 16:41:15 +0100

    Added validations for All models and took out capacity input in Course Controller and View

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :courses
   has_many :bookings, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_one_attached :photo
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :telephone, presence: true
+  # validates :email, presence: true, format: {
+  #  with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+  # }
+  validates_associated :courses
+  validates_associated :bookings
 end
