e395500560993ab6826979778cf621ba19c54839
Lily Walch <lily.walch@gmail.com>
2020-10-28 12:24:25 +0100

    migration and geocoder gem installed

--- app/models/course.rb
+++ app/models/course.rb
@@ -10,4 +10,7 @@ class Course < ApplicationRecord
 
   validates_associated :occurences
   validates :user, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
